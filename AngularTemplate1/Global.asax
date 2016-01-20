0777b10e850c0d4025ca78b51b81acd69e00418a
bachmeyer <wesyb@yahoo.com>
2016-01-19 20:48:57 -0600

    upload second angular template, possible duplicate

---
+++ AngularTemplate1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularTemplate1.WebApiApplication" Language="C#" %>
---
+++ AngularTemplate1/ngApp/app.ts
@@ -0,0 +1,12 @@
+﻿namespace AngularTemplate1 {
+
+    angular.module("AngularTemplate1", ['ngRoute'])
+        .config(function($routeProvider) {
+            //$routeProvider.when('/', {
+            // templateUrl: '/path/to/view',
+            // controller: AngularTemplate1.Controllers.ControllerClass,
+            // controllerAs: 'views variable name for controller'
+            //});
+            $routeProvider.when('/', { template: 'Hello World!' });
+        });
+}
